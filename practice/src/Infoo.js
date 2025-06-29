8703bdac8139f333b480b7a8216c0fb9d0b713d7
LarryChen <larry110864gmail>
2025-06-29 14:45:16 +0800

    JSX語法第二部分
    css import、inline、camelCase

--- practice/src/Infoo.js
+++ practice/src/Infoo.js
@@ -1,14 +1,14 @@
 import React from "react";
+import "./styles/style.css";
 
 const Info = () => {
   let friends = ["小明", "小華", "小張"];
   return (
-    <div>
+    <div className="info">
       <p>我的朋友們是:</p>
       {friends.map((friend) => {
         return <p>{friend}</p>;
       })}
-      <h1>這是Info組件</h1>
     </div>
   );
 };
